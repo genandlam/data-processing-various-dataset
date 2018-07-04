9f106d6a48546b561bae0e876b9c8cddf57dd8a9
Genevieve <genandlam@gmail.com>
2018-07-04 09:42:30 +0800

    update movies

--- MovieDiC_V2/convert_xml.py
+++ MovieDiC_V2/convert_xml.py
@@ -30,21 +30,23 @@ def create_movie_txt(root,vulgar_words):
         for dialogue in root.findall('./movie[@id="'+str(count)+'"]/dialogue'):
     #    for dialogue in root.findall('./movie[@id="'+'615'+'"]/dialogue'):
             speaker=dialogue.findall('speaker')
-
+            
             utterance=dialogue.findall('utterance')
             speaker_list=[]
-
+            
+        
             for row_num in range(0,len(speaker)):
 
                check=''
                utter=[]
                utter=[word for word in (utterance[row_num].text.lower().split()) if not word in vulgar_words]
                utter = ' '.join((utter)).encode('utf-8').strip()
-
+               speaker_name= (speaker[row_num].text).encode('utf-8').strip()
+              
                if  row_num==0:
 
                    speaker_list.append(speaker[row_num].text)
-                   f.write("speaker1: %s \n" % (utter))
+                   f.write("%s: %s \n" % (speaker_name,utter))
     #               print("speaker1: %s %s " % (speaker_list[0],utter))
 
                if row_num!=0:
@@ -53,13 +55,13 @@ def create_movie_txt(root,vulgar_words):
 
                        if speaker[row_num].text ==speaker_list[row]:
                            check='yes'
-                           f.write("speaker%d: %s\n"%(row+1,utter))
+                           f.write("%s: %s\n"%(speaker_name,utter))
     #                       print("speaker%d: %s %s"%(row+1,speaker_list[row],utter))
 
                    if  row==(len(speaker_list)-1) and speaker_list[row]!=speaker[row_num].text and check=='':
 
                        speaker_list.append(speaker[row_num].text)
-                       f.write("speaker%d: %s\n"%(row+2,utter))
+                       f.write("%s: %s\n"%(speaker_name,utter))
      #                  print("speaker%d: %s %s"%(row+2,speaker[row_num].text,utter))
 
             f.write('\n')
@@ -67,7 +69,18 @@ def create_movie_txt(root,vulgar_words):
         f.close()
         count+=1
 
-
+def count_session(root):
+        
+    count=1
+    counter=0
+    while (count<616)  :  
+        
+        for dialogue in root.findall('./movie[@id="'+str(count)+'"]/dialogue'):
+            counter+=1
+         
+        count+=1
+    return counter
+    
 #    for speaker in speaker1:
 #        speakers.append(speaker.text)
 #    for utter in utterance:
@@ -87,10 +100,12 @@ if __name__ == '__main__':
     #print child attrib movie
 #    print_child(root)
 
-    if not os.path.exists('./data/tmp/'):
-        os.makedirs('./data/tmp/')
+#    if not os.path.exists('./data/tmp/'):
+#        os.makedirs('./data/tmp/')
 
     create_movie_txt(root,vulgar_words)
+    
+#    counter=count_session(root)
 
 
 
